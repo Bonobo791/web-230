6005b8f5baabade31f086bceccbe2427c94cb82b
Bonobo791 <andrew.weilbacher@gmail.com>
2019-05-30 14:17:05 -0400

    Update AndrewWeilbacher_Discussion1_JS_Errors.js

--- AndrewWeilbacher_Discussion1_JS_Errors.js
+++ AndrewWeilbacher_Discussion1_JS_Errors.js
@@ -4,6 +4,8 @@ for (i = 0; i < 5; i+) {
   text = "The number is " + i + "<br>";
 }
 
-//expected result is printing "the number is ..." until 5
+// expected result is printing "the number is ..." until 5
+
+// there are two errors to fix
 
 // https://www.w3schools.com/jsref/jsref_for.asp
