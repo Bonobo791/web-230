cb2dae57865220e6e56c53e0ffe1272041fc90be
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-01 18:42:53 -0400

    update

---
+++ web-230/AndrewWeilbacher_Discussion1_JS_Errors.js
@@ -0,0 +1,11 @@
+var text = "";
+var i;
+for (i = 0; i < 5; i+) {
+  text = "The number is " + i + "<br>";
+}
+
+// expected result is printing "the number is ..." until 5
+
+// there are two errors to fix
+
+// https://www.w3schools.com/jsref/jsref_for.asp
---
+++ web-230/README.md
@@ -0,0 +1,6 @@
+# web-230
+Introduction to JavaScript
+
+#contributors
+Professor Krasso - Bellevue University
+Andrew Weilbacher - Bellevue University
