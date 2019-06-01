feec3bde71465e2b3cdef53a55cd47cfb8c18a9f
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-01 18:39:48 -0400

    update

--- week-1/Weilbacher-Assignment-1-2.js
+++
@@ -1,8 +0,0 @@
-var firstName = "Andrew";
-var lastName = "Weilbacher";
-var age = "27";
-var street = "555 Maldito Ave";
-var city = "Tampa";
-var zipCode = "33811";
-
-console.log(firstName, lastName, age, street, city, zipCode);
