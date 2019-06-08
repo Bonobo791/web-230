071b13bea41ec2f504cc20ff6b982323f3350661
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-08 11:16:09 -0400

    changed folder name

---
+++ web-231/week-1/Weilbacher-Assignment-1-2.js
@@ -0,0 +1,8 @@
+var firstName = "Andrew";
+var lastName = "Weilbacher";
+var age = "27";
+var street = "555 Maldito Ave";
+var city = "Tampa";
+var zipCode = "33811";
+
+console.log(firstName, lastName, age, street, city, zipCode);
