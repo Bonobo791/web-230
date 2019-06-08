12c2c5862fb7e93f82998f065ab0a91d626d54e2
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-08 11:38:30 -0400

    test

---
+++ web-231/week-2/Weilbacher-header-test.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   08 June 2019
+; Modified By: Andrew Weilbacher
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+* Params: firstName, lastName, assignment
+* Response: output
+* Description: Returns a well-formatted string header
+*/
+
+const header = require('../Weilbacher-header.js');
