1f1cb29655d3f11c3b12e9d3a17d646866942f94
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-15 15:16:59 -0400

    exercise 3.2

---
+++ web-231/week-3/Weilbacher-Exercise-3.2.js
@@ -0,0 +1,102 @@
+/*
+============================================
+; Title:  Weilbacher-Exercise-3.2.js
+; Author: Andrew Weilbacher
+; Date:   15 June 2019
+; Description: Displays a formatted header and example function for assignment
+;===========================================
+*/
+
+// header
+  const header = require('./Weilbacher-header.js');
+
+  console.log(header.display("Andrew", "Weilbacher", "Exercise 3.2"));
+
+// start program
+
+/*
+  Expected output:
+  FirstName LastName
+  Exercise 3.2
+  Today's Date
+
+  // output from the match() function
+  false
+  true
+
+  // output from the if...else blocks
+  Truck and Car do not match!
+  Bike and Bike do match!
+  Four and Three do not match!
+*/
+
+// variables
+  var testVar1 = "Truck";
+  var testVar2 = "Car";
+  var testVar3 = "Bike";
+  var testVar4 = "Bike";
+  var testVar5 = "Four";
+  var testVar6 = "Three";
+
+// functions
+
+  // function match
+  function match(arg1, arg2) {
+    // compare arguments for equality
+    if (arg1 === arg2)
+    // output evaluation of arguments
+      return true
+    else
+      return false
+  }
+
+  // function logMismatch
+  function logMismatch(arg1, arg2) {
+    // log argument variables into a string if match
+    console.log(arg1 + " and " + arg2 + " do not match!");
+  }
+
+  // function logMatch
+  function logMatch(arg1, arg2) {
+    // log argument variables into a string if match
+    console.log(arg1 + " and " + arg2 + " do match!");
+  }
+
+// Output from the match() function
+  // Test strings in match function for true or false values
+  console.log(match("A","B"));
+  console.log(match(2,2));
+
+// Conditions if...else statements. Include checks for all six (6) test variables
+
+  // test testVar1 and testVar2
+    // enter testVar1 and testVar2 into match function to evaluate
+    if (match(testVar1, testVar2)) {
+        // test variables if match
+        logMatch(testVar1, testVar2);
+      } else {
+        // else test variables if mismatch
+        logMismatch(testVar1, testVar2);
+      }
+
+  // test testVar3 and testVar4
+    // enter testVar3 and testVar4 into match function to evaluate
+    if (match(testVar3, testVar4)) {
+      // test variables if match
+      logMatch(testVar3, testVar4);
+    } else {
+      // else test variables if mismatch
+      logMismatch(testVar3, testVar4);
+    }
+
+  // test testVar5 and testVar6
+    // enter testVar5 and testVar6 into match function to evaluate
+    if (match(testVar5, testVar6)) {
+      // test variables if match
+      logMatch(testVar5, testVar5);
+    } else {
+      // else test variables if mismatch
+      logMismatch(testVar5, testVar6);
+    }
+
+// end program
