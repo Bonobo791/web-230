a2803b86234e5ad69ecf3acb3566b7481099babe
Andrew Weilbacher <andrew.weilbacher@gmail.com>
2019-06-15 14:29:35 -0400

    discussion 1

---
+++ web-231/week-1/Weilbacher-Discussion-1-Comlan.js
@@ -0,0 +1,21 @@
+/*
+============================================
+* Title: Discussion Board Assignment 1.1
+* Author: Comlan KOUHIKO
+* Date: May 30, 2019
+* Modified By: Andrew Weilbacher
+* Description: A short program with three errors. When fixed
+* and executed, should give the following output:
+* "My name is Comlan KOUHIKO and I am a student at BELLEVUE University."
+===========================================
+*/
+
+// variables declaration
+var halfsent1 = "My name is ";
+var halfsent2 = "Comlan KOUHIKO";
+var halfsent3 = "and I am a student at BELLEVUE University.";
+var name = halfsent1 + halfsent2 + halfsent3;
+console.log(name);
+// end of variables declaration
+// output
+// document.write(name);
